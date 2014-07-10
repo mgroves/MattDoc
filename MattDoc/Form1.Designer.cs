bd0ee872fc4ea9adaac5a8806e2aeaeb75d6b786
mgroves <me@mgroves.com>
2014-07-10 08:18:59 -0400

    initial commit, moving over from svn

---
+++ MattDoc/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MattDoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePickerDialog = new System.Windows.Forms.OpenFileDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.goButton = new System.Windows.Forms.Button();
+            this.fileList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // filePickerDialog
+            // 
+            this.filePickerDialog.Multiselect = true;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(326, 25);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(326, 279);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(107, 23);
+            this.goButton.TabIndex = 2;
+            this.goButton.Text = "Save To 1 PDF";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // fileList
+            // 
+            this.fileList.FormattingEnabled = true;
+            this.fileList.Location = new System.Drawing.Point(13, 25);
+            this.fileList.Name = "fileList";
+            this.fileList.Size = new System.Drawing.Size(307, 277);
+            this.fileList.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 326);
+            this.Controls.Add(this.fileList);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.browseButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog filePickerDialog;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.ListBox fileList;
+    }
+}
+
---
+++ MattDoc/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace MattDoc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            filePickerDialog.ShowDialog();
+            if(filePickerDialog.FileNames != null && filePickerDialog.FileNames.Any())
+            {
+                filePickerDialog.FileNames.ToList().ForEach(f => fileList.Items.Add(f));
+            }
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            if (fileList.Items == null || fileList.Items.Count <= 0) return;
+
+            var doc = new PdfDocument();
+            doc.Info.Title = "Matt Doc 0.1";
+
+            foreach (var item in fileList.Items)
+            {
+                var filename = (string) item;
+                if(filename.EndsWith(".pdf"))
+                {
+                    AddPdf(filename, doc);
+                } else
+                {
+                    AddImage(filename, doc);
+                }
+            }
+
+            var saveFile = PromptForFilenameSave();
+
+            doc.Save(saveFile);
+        }
+
+        private string PromptForFilenameSave()
+        {
+            filePickerDialog.CheckFileExists = false;
+            filePickerDialog.Multiselect = false;
+            filePickerDialog.ShowDialog();
+            filePickerDialog.CheckFileExists = true;
+            filePickerDialog.Multiselect = true;
+            return filePickerDialog.FileName;
+        }
+
+        private void AddImage(string filename, PdfDocument doc)
+        {
+            var page = doc.AddPage();
+
+            var gfx = XGraphics.FromPdfPage(page);
+
+            var img = XImage.FromFile(filename);
+
+            gfx.DrawImage(img, 0, 0);
+        }
+
+        private void AddPdf(string filename, PdfDocument doc)
+        {
+            using (var inputDoc = PdfReader.Open(filename, PdfDocumentOpenMode.Import))
+            {
+                for (int i = 0; i < inputDoc.PageCount; i++)
+                {
+                    doc.AddPage(inputDoc.Pages[i]);
+                }
+            }
+        }
+    }
+}
